a1c39609e78b9b9c276578005f98baa339cae21e
AntoineF <antoine.frayard@frd-solution.fr>
2024-11-16 00:14:38 +0100

    first commit

---
+++ Debug/Core/Inc/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Inc/dwt_stm32_delay.c 
+
+OBJS += \
+./Core/Inc/dwt_stm32_delay.o 
+
+C_DEPS += \
+./Core/Inc/dwt_stm32_delay.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Inc/%.o Core/Inc/%.su Core/Inc/%.cyclo: ../Core/Inc/%.c Core/Inc/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Inc
+
+clean-Core-2f-Inc:
+	-$(RM) ./Core/Inc/dwt_stm32_delay.cyclo ./Core/Inc/dwt_stm32_delay.d ./Core/Inc/dwt_stm32_delay.o ./Core/Inc/dwt_stm32_delay.su
+
+.PHONY: clean-Core-2f-Inc
+
